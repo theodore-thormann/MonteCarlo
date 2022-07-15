fcd19efc0a5ed67b9310033fec749246b6c4b96e
theodore-thormann <50964270+theodore-thormann@users.noreply.github.com>
2022-07-15 10:23:03 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MonteCarlo',
+      version='0.1',
+      description='A Monte Carlo simulator',
+      url='http://github.com/theodore-thormann/MonteCarlo',
+      author='Theo Thormann',
+      author_email='nbx5kp@virginia.edu',
+      license='MIT',
+      packages=['MonteCarlo'])
